046dd4ebb2d66a4c827ec40545d6ae71cb768070
Ahmed Samy <f.fallen45@gmail.com>
2013-09-09 07:12:41 +0000

    License everywhere.

--- net/inputmessage.cpp
+++ net/inputmessage.cpp
@@ -1,14 +1,26 @@
+/*
+ * Copyright (c) 2013 Ahmed Samy  <f.fallen45@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 #include "inputmessage.h"
-
-static __inline uint16_t unpack_u16(unsigned char *addr)
-{
-	return addr[1] << 8 | addr[0];
-}
-
-static __inline uint32_t unpack_u32(unsigned char *addr)
-{
-	return unpack_u16(addr + 2) << 16 | unpack_u16(addr);
-}
+#include "../util/math.h"
 
 InputMessage::InputMessage(uint8_t *data, size_t size) :
 	m_data(data),
@@ -37,14 +49,14 @@ uint8_t InputMessage::getByte()
 
 uint16_t InputMessage::getU16()
 {
-	uint16_t tmp = unpack_u16(&m_data[m_pos]);
+	uint16_t tmp = readLE16(&m_data[m_pos]);
 	m_pos += 2;
 	return tmp;
 }
 
 uint32_t InputMessage::getU32()
 {
-	uint32_t tmp = unpack_u32(&m_data[m_pos]);
+	uint32_t tmp = readLE32(&m_data[m_pos]);
 	m_pos += 4;
 	return tmp;
 }
